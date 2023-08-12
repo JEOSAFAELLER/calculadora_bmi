ad462c98b8211afb78ea6d2a4268b6efb7ccc54a
jeosafa eller <jeosafaeller@gmail.com>
2023-08-12 20:16:49 -0300

    input text feat!

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
 import React, {useState} from 'react'
 
 const BmiCalculator = () => {
@@ -31,7 +31,25 @@ const BmiCalculator = () => {
 
   return (
     <View style= {styles.container}>
-      <Text>BmiCalculator</Text>
+        <View style={styles.title}>
+        <Text style={styles.titleText}>BMI Calculator</Text>
+
+        </View>
+        <TextInput
+        style={styles.input}
+        value={weight}
+        onChangeText={(text) => setWeight(text)}
+        placeholder='Peso em Kg'
+        keyboardType='numeric' 
+        />
+         <TextInput
+        style={styles.input}
+        value={height}
+        onChangeText={(text) => setheight(text)}
+        placeholder='Altura em cm'
+        keyboardType='numeric' 
+        />
+      
     </View>
   )
 }
@@ -44,8 +62,29 @@ export default BmiCalculator
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      paddingTop: Constants.statusBarHeight,
+      backgroundColor:'#e0ecde'
     },
+    title:{
+        backgroundColor:'#2c6975',
+        height: 80,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom:10
+
+    },
+    titleText:{
+        fontSize: 30,
+        color:'#fff',
+        fontWeight: 'bold'
+    },
+    input:{
+        height:55,
+        margin:15,
+        borderWidth:1/2,
+        padding:10,
+        borderRadius:5,
+        backgroundColor:"#cde0c9",
+        fontSize:18
+    }
   });
